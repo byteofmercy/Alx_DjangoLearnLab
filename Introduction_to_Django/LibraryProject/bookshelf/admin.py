f1baf7dee9414e8c6fe31e46a820a67b859b4bd7
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-08-30 23:36:42 +0300

    Register Book model in admin, customize admin list display, and add CRUD documentation

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # columns to show in admin list
+    list_filter = ('publication_year', 'author')            # filters on the right sidebar
+    search_fields = ('title', 'author')                     # search box fields
 
+admin.site.register(Book, BookAdmin)
