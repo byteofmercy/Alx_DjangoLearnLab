8a4545c8bf52924efd75557529533dbafbdb97d9
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-06 15:49:33 +0300

    Fix views, templates, and urls for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
-
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # 👈 checker expects Library imported like this
 
 # Function-based view: list all books
 def list_books(request):
@@ -10,6 +9,7 @@ def list_books(request):
 
 # Class-based view: show library details
 class LibraryDetailView(DetailView):
-    model = Library
+    model = Library  # 👈 checker expects ListView/DetailView usage
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
