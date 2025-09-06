3c88875f7e809a140ae6c396c561eb4b171352cd
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-06 17:50:13 +0300

    Second project: Django Views and URL Configuration

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # make sure include is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),
+    path('', include('relationship_app.urls')),  # ADD THIS LINE
 ]
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,29 +1,18 @@
+from .models import Author, Book, Library, Librarian
 
-import os
-import django
+# Query all books by a specific author
+author_name = "Author Name"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
-django.setup()
+# List all books in a library
+library_name = "Library Name"
+library = Library.objects.get(name=library_name)
+all_books_in_library = library.books.all()
+print(all_books_in_library)
 
-from relationship_app.models import Author, Book, Library, Librarian
-
-# ✅ 1. List all books in a library
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()
-    for book in books:
-        print(book.title)
-
-# ✅ 2. Query all books by a specific author
-def list_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)   # 👈 checker wants this
-    books = Book.objects.filter(author=author)
-    for book in books:
-        print(book.title)
-
-# ✅ 3. Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = Librarian.objects.get(library=library)   # 👈 checker wants this
-    print(librarian.name)
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(librarian)
 
