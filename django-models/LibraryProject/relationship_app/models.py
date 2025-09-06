5994deff1037dab65fc43d6629196f960bc78537
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-06 16:38:27 +0300

    Add FBV and CBV views, templates, URLs, and migration for LibraryProject

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,31 +3,18 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    # FIX: added related_name="books" so we can use author.books.all()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField(default=2024)
     def __str__(self):
         return self.title
 
-
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
+    books = models.ManyToManyField(Book, blank=True)
     def __str__(self):
         return self.name
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
-from .models import Book, Library  # 👈 checker expects Library imported like this
 
-# Function-based view: list all books
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view for all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'list_books.html', {'books': books})
 
-# Class-based view: show library details
+# Class-based view for library detail
 class LibraryDetailView(DetailView):
-    model = Library  # 👈 checker expects ListView/DetailView usage
-    template_name = 'relationship_app/library_detail.html'
+    model = Library
+    template_name = 'library_detail.html'
     context_object_name = 'library'
-
