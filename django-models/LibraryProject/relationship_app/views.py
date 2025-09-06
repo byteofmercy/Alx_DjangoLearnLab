cc3f46bdfeff262d7c01cc69ac68aa14c25c88b5
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-06 14:57:06 +0300

    Add views, urls, and templates for books and library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # 👈 checker expects this
 
-# Function-based view: list all books (title and author)
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(request, 'list_books.html', {'books': books})
 
-# Class-based view: show a single library and its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
