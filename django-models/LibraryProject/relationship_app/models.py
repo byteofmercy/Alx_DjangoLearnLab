0c97a01a9988f389bf4bbe2ebf0407a95f0d5a29
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 15:59:36 +0300

    Update models and query_samples for advanced relationships

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,31 +1,33 @@
+
 from django.db import models
 
+# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-
+# Library model
 class Library(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-
+# Book model
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-
+# Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,23 +1,29 @@
-import django
 import os
+import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Library, Book, Author, Librarian
 
+# ------------------------------
 # List all books in a library
+# ------------------------------
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
-books_in_library = [book.title for book in library.books.all()]
-print(f"Books in {library_name}: {books_in_library}")
+books_in_library = library.books.all()  # uses related_name='books'
+print(f"Books in {library_name}: {[book.title for book in books_in_library]}")
 
+# ------------------------------
 # Query all books by a specific author
+# ------------------------------
 author_name = "Author A"
 author = Author.objects.get(name=author_name)
-books_by_author = [book.title for book in author.books.all()]
-print(f"Books by {author_name}: {books_by_author}")
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
+# ------------------------------
 # Retrieve the librarian for a library
-librarian = library.librarian
+# ------------------------------
+librarian = Librarian.objects.get(library=library)
 print(f"Librarian of {library_name}: {librarian.name}")
