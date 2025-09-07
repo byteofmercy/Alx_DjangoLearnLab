da1be85a5d5e7a9bec5a274e4736db78158fc393
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 21:12:55 +0300

    Fix class-based view and function-based view with correct Library import, update URLs and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
+# Import models and views
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book, Author
 
-# Function-based view for listing books
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view for library details
+# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
