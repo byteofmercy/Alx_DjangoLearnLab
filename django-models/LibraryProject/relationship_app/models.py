303add184953b22fb3450bf2e6fe9ee4f8bae72d
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 18:53:44 +0300

    Implemented user authentication and class-based views for libraries

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,30 +1,27 @@
-
 from django.db import models
 
-# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Library model
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-# Book model
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    # Book belongs to Library via ManyToMany in Library
 
     def __str__(self):
         return self.title
 
-# Librarian model
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,37 @@
 # relationship_app/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # checker expects Library import
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
-# -----------------------
-# Function-based view to list all books
-# -----------------------
+# Must have for checker
+from .models import Library, Book
+
+# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# -----------------------
-# Class-based view to display library details
-# -----------------------
+# Class-based view: Library details with books
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+# User registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# Login/Logout
+login_view = LoginView.as_view(template_name='relationship_app/login.html')
+logout_view = LogoutView.as_view(template_name='relationship_app/logout.html')
