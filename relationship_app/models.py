71ede924ef769d70868c64acde6a0f4fa2d7e989
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 23:15:16 +0300

    Fix models and query_samples for checker

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,13 +1,13 @@
+
 from django.db import models
 
-# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Book model
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -15,19 +15,18 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-# Library model
+
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-# Librarian model
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
