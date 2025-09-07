6855a2c90c202f96af9d8a0213111357a3bc40d9
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 18:57:39 +0300

    Fixed imports and added templates to satisfy checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
+    # Function-based view
+    path('books/', list_books, name='list_books'),
+
+    # Class-based view
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication views
+    path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
+
 
