63373a56b990a941c29ecf8406fc74ac7ca5e947
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 12:16:39 +0300

    Remove old bookshelf app

--- bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns to show in admin list
-    list_filter = ('publication_year', 'author')            # filters on the right sidebar
-    search_fields = ('title', 'author')                     # search box fields
-
-admin.site.register(Book, BookAdmin)
