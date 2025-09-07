295ae618a2d7f77bbc5f488c324d76221e00025c
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 10:26:28 +0300

    Remove old Project 2 views, URLs, templates, and migrations for fresh start

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,49 +1,17 @@
-
 from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-<<<<<<< HEAD
-
-    def __str__(self):
-        return self.name
-
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    # FIX: added related_name="books" so we can use author.books.all()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
-    def __str__(self):
-        return self.title
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
-    def __str__(self):
-        return self.name
-
-
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-=======
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    publication_year = models.IntegerField(default=2024)
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, blank=True)
->>>>>>> ead0755 (Second project: Django Views and URL Configuration)
-    def __str__(self):
-        return self.name
