57434a76077e33460edbd0529cc85f64b5984b23
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-07 12:38:02 +0300

    Completely remove old LibraryProject to start fresh

--- django-models/LibraryProject/LibraryProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # make sure include is imported
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # ADD THIS LINE
-]
