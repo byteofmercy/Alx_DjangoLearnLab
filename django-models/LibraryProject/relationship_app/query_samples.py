b1193e2d342abe5961bb36865f18eb441e39e5aa
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-08 14:47:38 +0300

    Update query_samples.py to match checker patterns

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 # List all books in a library
 def books_in_library(library_name):
@@ -12,4 +13,5 @@ def books_in_library(library_name):
 # Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian.name
+    return library.librarian
+
