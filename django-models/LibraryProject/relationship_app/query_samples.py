50529d28e5a7090d4bb1d67e2ec58cce8c986370
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-08 14:44:48 +0300

    Fix query_samples.py: add books_in_library and librarian_for_library

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,17 +1,15 @@
+from .models import Author, Book, Library, Librarian
 
-from relationship_app.models import Library, Book, Author, Librarian
-
-# 1. List all books in a library.
-def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)   # required by checker
-    return Book.objects.filter(library=library)
-
-# 2. Query all books by a specific author.
+# Query all books by a specific author
 def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    return Book.objects.filter(author__name=author_name)
 
-# 3. Retrieve the librarian for a library.
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)   # required by checker
-    return Librarian.objects.get(library=library)
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian.name
