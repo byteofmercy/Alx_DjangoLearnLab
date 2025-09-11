f7d57282963f63338310c8fa2d26c2522bde3af0
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-11 13:40:42 +0300

    Add custom user model (accounts.CustomUser) with date_of_birth and profile_photo

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,28 +1,28 @@
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+        return self.username
