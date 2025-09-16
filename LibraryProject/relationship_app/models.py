efa8f31606cd05ff50f0ce75fc4e3ca2321fb722
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-16 13:34:24 +0300

    Add Library, Book, and Librarian models with sample queries

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,32 +1,17 @@
 from django.db import models
 
-# Author model
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-# Book model
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-# Library model
 class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# Librarian model
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='librarians')
 
     def __str__(self):
         return self.name
