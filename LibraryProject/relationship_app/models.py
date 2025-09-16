99ae19a49d7efb35c4d6411defd80bbd6f64c485
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-16 13:41:10 +0300

    Add Librarian model and run migrations

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -7,11 +7,10 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
-
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='librarians')
+    email = models.EmailField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
