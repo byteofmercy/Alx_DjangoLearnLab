4c79e0e2ff6a71ef0b82f689690591b86fbd11ee
Mercy Nzioka <mercyndunge2001@gmail.com>
2025-09-19 18:41:44 +0300

    Add book_list view with permission decorator and books queryset

--- LibraryProject/bookshelf/views.py
+++ LibraryProject/bookshelf/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 
@@ -7,29 +7,3 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-@permission_required('bookshelf.can_create', raise_exception=True)
-def book_create(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        Book.objects.create(title=title, author=author)
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html')
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def book_edit(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.title = request.POST.get('title')
-        book.author = request.POST.get('author')
-        book.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html', {'book': book})
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def book_delete(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
